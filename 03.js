0d432f8c0ed6b49b0b13a15c9e603bb85d527fc9
naomiaputri <46338014+naomiaputri@users.noreply.github.com>
2020-02-14 21:31:11 +0700

    .

--- 03.js
+++ 03.js
@@ -1,9 +1,8 @@
-<script>
+
 var input = document.getElementById('input');
 var mirror = document.getElementById('mirror');
 
 input.addEventListener('input', function(event) {
   mirror.innerText = event.target.value.split('').reverse().join('');
 });
-</script>
 
