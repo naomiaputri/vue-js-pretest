8e56fa08f207e902fbfd6870af6fdbbcec527d29
naomiaputri <46338014+naomiaputri@users.noreply.github.com>
2020-02-14 21:32:26 +0700

    Update 02.js

--- 02.js
+++ 02.js
@@ -1,8 +1,7 @@
-<script>
+
 var word = ["saya", "ingin", "makan", "nasi", "goreng"];
 document.getElementById("test").innerHTML = word;
 
 function myFunction() {
   document.getElementById("test").innerHTML = word.reverse().join(" ");
 }
-</script>
